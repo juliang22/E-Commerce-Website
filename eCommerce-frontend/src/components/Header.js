fa70a9842ff12bfa13331b27b734fef59bff976f
Julian Grunauer <Julian.A.Grunauer.21@Dartmouth.edu>
2020-12-18 10:52:55 -0800

    name change

--- eCommerce-frontend/src/components/Header.js
+++ eCommerce-frontend/src/components/Header.js
@@ -53,7 +53,7 @@ const Header = () => {
 			<Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
 				<Container>
 					<LinkContainer to='/'>
-						<Navbar.Brand>ProShop</Navbar.Brand>
+						<Navbar.Brand>JulianShop</Navbar.Brand>
 					</LinkContainer>
 					<Navbar.Toggle aria-controls="basic-navbar-nav" />
 					<Navbar.Collapse id="basic-navbar-nav">
